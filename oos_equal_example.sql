c06e92f13ef7793f22248166c85faa702f4bb092
Jani Hur <webmaster@jani-hur.net>
2016-12-23 09:27:52 +0200

    Null aware equality (nulls are equal).

--- oos_equal_example.sql
+++ oos_equal_example.sql
@@ -1,12 +1,25 @@
+declare
+  function to_str(p_a in boolean) return varchar2 is
+  begin
+    return
+      case p_a
+        when true then 'TRUE'
+        when false then 'FALSE'
+        else 'NULL'
+      end;
+  end;
+  procedure test(p_a in number, p_b in number) is
+    v_res boolean := oos_equal.is_(p_a, p_b);
+  begin
+    dbms_output.put_line(
+      '(p_a ' || p_a || ')(p_b ' || p_b || ')(v_res ' || to_str(v_res) || ')'
+    );
+  end;
 begin
-  if oos_equal.is_(to_number(null), to_number(null))
-  then
-    dbms_output.put_line('number values are equal');
-  end if;
-
-  if oos_equal."not"(1, null)
-  then
-    dbms_output.put_line('number values are not equal');
-  end if;
+  test(to_number(null), to_number(null));
+  test(null, 1);
+  test(1, null);
+  test(1, 1);
+  test(1, 2);
 end;
 /
